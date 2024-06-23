529c91e586c12ad68a7d87801b478d6f0a352704
mohamedsharrif <sharifmohamedmahamud@gmail.com>
2024-06-23 14:51:45 +0300

    3.16: Phonebook database, step 4

--- part3/backend-phonebook/index.js
+++ part3/backend-phonebook/index.js
@@ -56,6 +56,23 @@ app.delete('/api/persons/:id', (req, res, next) => {
     .catch(error => next(error))
 })
 
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
+
 
 
 
